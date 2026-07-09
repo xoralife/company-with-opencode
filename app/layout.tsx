cca3c1c032d9afd565b5c92539189930d39fbd65
xoralife <abidhussainiouu@gmail.com>
2026-07-09 15:27:14 +0500

    Fix hydration error - add suppressHydrationWarning to html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`}>
+    <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`} suppressHydrationWarning>
       <body className="min-h-screen flex flex-col antialiased">
         <Header />
         <main className="flex-1">{children}</main>
